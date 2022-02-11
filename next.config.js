b1c65e306418186e20218011a3e65477b7942088
m7he4rt <mateuspm2015@hotmail.com>
2022-02-11 20:43:54 -0300

    feat: adding dragger and func csv to table

--- next.config.js
+++ next.config.js
@@ -1,6 +1,14 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {
-  reactStrictMode: true,
-}
+const withAntdLess = require("next-plugin-antd-less")
 
-module.exports = nextConfig
+module.exports = withAntdLess({ 
+  modifyVars: { '@primary-color': '#7159c1' },
+  lessVarsFilePathAppendToEndOfContent: true,
+
+  nextjs: {
+    localIdentNameFollowDev: true
+  },
+
+  webpack(config) {
+    return config
+  }
+})
